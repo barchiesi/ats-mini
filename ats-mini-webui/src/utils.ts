5990e696e57f9a05f95bcc5ae2520a02bb4b18db
David Barchiesi <david@barchie.si>
2025-07-12 19:30:22 +0200

    Recreate static web server pages in dedicated npm project with autorefresh for status and memory.

---
+++ ats-mini-webui/src/utils.ts
@@ -0,0 +1,50 @@
+export const responseToJson = async (response: Response) => {
+  if (!response.ok) {
+    const errorText = await response.text();
+    throw new Error(`HTTP error status: ${response.status}, body: ${errorText}`);
+  }
+  return response.json();
+}
+
+export const setCellText = (id: string, text: string) => {
+  const cell = document.getElementById(id);
+  if (cell) cell.textContent = text;
+}
+
+export const inputValue = <T extends HTMLInputElement | HTMLSelectElement>(id: string): string => (document.getElementById(id) as T).value
+export const setInputValue = <T extends HTMLInputElement | HTMLSelectElement>(id: string, value: string) => {
+  (document.getElementById(id) as T).value = value
+}
+
+export const checkboxValue = (id: string): boolean => (document.getElementById(id) as HTMLInputElement).checked
+export const setCheckboxValue = (id: string, checked: boolean) => {
+  (document.getElementById(id) as HTMLInputElement).checked = checked
+}
+
+export const populateSelect = (selectId: string, options: { value: string, label: string }[]) => {
+  const select = document.getElementById(selectId);
+  if (!select) {
+    return;
+  }
+
+  options.forEach((item) => {
+    const option = document.createElement('option');
+    option.value = item.value.toString();
+    option.textContent = item.label;
+    select.appendChild(option);
+  })
+}
+
+export const syncValues = (srcInputId: string, destSpanId: string) => {
+  const srcInput = document.getElementById(srcInputId) as HTMLInputElement;
+  const destSpan = document.getElementById(destSpanId) as HTMLSpanElement;
+  if (srcInput && destSpan) {
+    srcInput.addEventListener('input', () => {
+      destSpan.textContent = srcInput.value;
+    });
+  }
+}
+
+export const formatFrequency = (frequencyHz: number, mode: string = "AM"): string => {
+  return mode === "FM" ? `${(frequencyHz / 1000 / 1000).toFixed(2)}MHz` : `${(frequencyHz / 1000).toFixed(3)}kHz`
+}
