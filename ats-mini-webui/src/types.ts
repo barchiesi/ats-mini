34f2d23d6201455b16a245a425afac433e8ac0e9
David Barchiesi <david@barchie.si>
2025-07-12 19:30:22 +0200

    Recreate static web server pages in dedicated npm project with autorefresh for status and memory.

---
+++ ats-mini-webui/src/types.ts
@@ -0,0 +1,52 @@
+export interface Status {
+  ip: string;
+  ssid: string;
+  mac: string;
+  version: string;
+  band: string;
+  freq: number;
+  mode: string;
+  rssi: number;
+  snr: number;
+  battery: number;
+}
+
+export interface Memory {
+  id?: number;
+  freq?: number;
+  band?: string;
+  mode?: string;
+  name?: string;
+}
+
+export interface UTCOffset {
+  id: number;
+  offset: number;
+  desc: string;
+  city: string;
+}
+
+export interface Theme {
+  id: number;
+  name: string;
+}
+
+export interface ConfigOptions {
+  UTCOffsets: UTCOffset[];
+  themes: Theme[];
+}
+
+export interface Config {
+  username: string;
+  password: string;
+  wifissid1: string;
+  wifipass1: string;
+  wifissid2: string;
+  wifipass2: string;
+  wifissid3: string;
+  wifipass3: string;
+  utcOffsetIdx: number;
+  themeIdx: number;
+  scrollDirection: number;
+  zoomMenu: boolean;
+}
---
+++ ats-mini-webui/src/utils.ts
@@ -0,0 +1,52 @@
+export const byId = (id: string) => document.getElementById(id);
+
+export const responseToJson = async (response: Response) => {
+  if (!response.ok) {
+    const errorText = await response.text();
+    throw new Error(`HTTP error status: ${response.status}, body: ${errorText}`);
+  }
+  return response.json();
+}
+
+export const setCellText = (id: string, text: string) => {
+  const cell = byId(id);
+  if (cell) cell.textContent = text;
+}
+
+export const inputValue = <T extends HTMLInputElement | HTMLSelectElement>(id: string): string => (byId(id) as T).value
+export const setInputValue = <T extends HTMLInputElement | HTMLSelectElement>(id: string, value: string) => {
+  (byId(id) as T).value = value
+}
+
+export const checkboxValue = (id: string): boolean => (byId(id) as HTMLInputElement).checked
+export const setCheckboxValue = (id: string, checked: boolean) => {
+  (byId(id) as HTMLInputElement).checked = checked
+}
+
+export const populateSelect = (selectId: string, options: { value: string, label: string }[]) => {
+  const select = byId(selectId);
+  if (!select) {
+    return;
+  }
+
+  select.replaceChildren(...options.map(i => {
+    const option = document.createElement('option');
+    option.value = i.value;
+    option.textContent = i.label;
+    return option;
+  }))
+}
+
+export const syncValues = (srcInputId: string, destSpanId: string) => {
+  const srcInput = byId(srcInputId) as HTMLInputElement;
+  const destSpan = byId(destSpanId) as HTMLSpanElement;
+  if (srcInput && destSpan) {
+    srcInput.addEventListener('input', () => {
+      destSpan.textContent = srcInput.value;
+    });
+  }
+}
+
+export const formatFrequency = (frequencyHz: number, mode: string = "FM", noUnit = false): string => {
+  return mode === "FM" ? `${(frequencyHz / 1000 / 1000).toFixed(2)}${!noUnit ? 'MHz' : ''}` : `${(frequencyHz / 1000).toFixed(3)}${!noUnit ? 'kHz' : ''}`
+}
