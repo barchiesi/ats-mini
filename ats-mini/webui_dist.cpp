fd0646320666db9909d9f0cd58300c95edfccf82
David Barchiesi <david@barchie.si>
2025-07-13 16:15:48 +0200

    Serve npm webui pages instead of static web server pages.

---
+++ ats-mini/webui_dist.cpp
@@ -0,0 +1,37 @@
+#include "webui_dist.h"
+
+WebUiFile webui_files[] =
+{
+  {"index.html",  "text/html", "<!DOCTYPE HTML>"
+    "<HTML>"
+    "<HEAD>"
+    "<META CHARSET='UTF-8'>"
+    "<META NAME='viewport' CONTENT='width=device-width, initial-scale=1.0'>"
+    "<TITLE>ATS-Mini webui Error</TITLE>"
+    "</HEAD>"
+    "<BODY STYLE='font-family: sans-serif;'>webui wasn't built correctly</BODY>"
+    "</HTML>"
+  },
+  {"memory.html",  "text/html", "<!DOCTYPE HTML>"
+    "<HTML>"
+    "<HEAD>"
+    "<META CHARSET='UTF-8'>"
+    "<META NAME='viewport' CONTENT='width=device-width, initial-scale=1.0'>"
+    "<TITLE>ATS-Mini webui Error</TITLE>"
+    "</HEAD>"
+    "<BODY STYLE='font-family: sans-serif;'>webui wasn't built correctly</BODY>"
+    "</HTML>"
+  },
+  {"config.html",  "text/html", "<!DOCTYPE HTML>"
+    "<HTML>"
+    "<HEAD>"
+    "<META CHARSET='UTF-8'>"
+    "<META NAME='viewport' CONTENT='width=device-width, initial-scale=1.0'>"
+    "<TITLE>ATS-Mini webui Error</TITLE>"
+    "</HEAD>"
+    "<BODY STYLE='font-family: sans-serif;'>webui wasn't built correctly</BODY>"
+    "</HTML>"
+  },
+};
+
+const int webui_files_count = 3;
