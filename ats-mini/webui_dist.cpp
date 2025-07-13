3d49f9ce0d63929d42a1e04457e192b654bd4962
David Barchiesi <david@barchie.si>
2025-07-13 16:15:48 +0200

    Serve npm webui pages instead of static web server pages.

---
+++ ats-mini/webui_dist.cpp
@@ -0,0 +1,37 @@
+#include "webui_dist.h"
+
+WebUiFile webui_files[] =
+{
+  {"index.html",  "text/html", "<!DOCTYPE HTML>"
+    "<HTML>"
+    "<HEAD>"
+    "<META CHARSET='UTF-8'>"
+    "<META NAME='viewport' CONTENT='width=device-width, initial-scale=1.0'>"
+    "<TITLE>ATS-Mini webui Error</TITLE>"
+    "</HEAD>"
+    "<BODY STYLE='font-family: sans-serif;'>webui wasn't built correctly, see <a href=\"https://barchiesi.github.io/ats-mini/development.html#adding-the-built-webui-to-the-firmware-sources\">the docs</a></BODY>"
+    "</HTML>"
+  },
+  {"memory.html",  "text/html", "<!DOCTYPE HTML>"
+    "<HTML>"
+    "<HEAD>"
+    "<META CHARSET='UTF-8'>"
+    "<META NAME='viewport' CONTENT='width=device-width, initial-scale=1.0'>"
+    "<TITLE>ATS-Mini webui Error</TITLE>"
+    "</HEAD>"
+    "<BODY STYLE='font-family: sans-serif;'>webui wasn't built correctly, see <a href=\"https://barchiesi.github.io/ats-mini/development.html#adding-the-built-webui-to-the-firmware-sources\">the docs</a></BODY>"
+    "</HTML>"
+  },
+  {"config.html",  "text/html", "<!DOCTYPE HTML>"
+    "<HTML>"
+    "<HEAD>"
+    "<META CHARSET='UTF-8'>"
+    "<META NAME='viewport' CONTENT='width=device-width, initial-scale=1.0'>"
+    "<TITLE>ATS-Mini webui Error</TITLE>"
+    "</HEAD>"
+    "<BODY STYLE='font-family: sans-serif;'>webui wasn't built correctly, see <a href=\"https://barchiesi.github.io/ats-mini/development.html#adding-the-built-webui-to-the-firmware-sources\">the docs</a></BODY>"
+    "</HTML>"
+  },
+};
+
+const int webui_files_count = 3;
