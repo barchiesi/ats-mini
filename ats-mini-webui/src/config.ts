019ae6a2a9ceebbc26180a814b70c843ded77bb2
David Barchiesi <david@barchie.si>
2025-08-01 22:58:39 +0200

    Add remaining missing editable configs to the web page.

--- ats-mini-webui/src/config.ts
+++ ats-mini-webui/src/config.ts
@@ -1,5 +1,5 @@
 import type {Config, ConfigOptions} from "./types";
-import {byId, checkboxValue, inputValue, populateSelect, setCheckboxValue, setInputValue} from "./utils";
+import {byId, checkboxValue, inputValue, populateSelect, setCheckboxValue, setInputValue, syncValues} from "./utils";
 import {configApi, configOptionsApi, saveConfigApi} from "./atsminiApi.ts";
 
 
@@ -13,10 +13,16 @@ const saveConfig = () => {
     wifipass2: inputValue('wifipass2'),
     wifissid3: inputValue('wifissid3'),
     wifipass3: inputValue('wifipass3'),
+    brightness: parseInt(inputValue('brightness')),
+    calibration: parseInt(inputValue('calibration')),
+    rdsModeIdx: parseInt(inputValue('rdsModes')),
     utcOffsetIdx: parseInt(inputValue('utcoffset')),
+    fmRegionIdx: parseInt(inputValue('fmRegions')),
     themeIdx: parseInt(inputValue('theme')),
-    scrollDirection: checkboxValue('scroll') ? -1 : 1,
+    uiLayoutIdx: parseInt(inputValue('uiLayouts')),
     zoomMenu: checkboxValue('zoom'),
+    scrollDirection: checkboxValue('scroll') ? -1 : 1,
+    sleepModeIdx: parseInt(inputValue('sleepModes')),
   };
 
   saveConfigApi(config)
@@ -35,10 +41,24 @@ const populateConfig = (config: Config) => {
   setInputValue('wifipass2', config.wifipass2);
   setInputValue('wifissid3', config.wifissid3);
   setInputValue('wifipass3', config.wifipass3);
+  setInputValue('wifipass3', config.wifipass3);
+  setInputValue('brightness', config.brightness.toString());
+
+  const brightnessValueSpan = byId('brightnessValue') as HTMLSpanElement;
+  if (brightnessValueSpan) brightnessValueSpan.textContent = config.brightness.toString();
+
+  setInputValue('calibration', config.calibration.toString());
+  const calibrationValueSpan = byId('calibrationValue') as HTMLSpanElement;
+  if (calibrationValueSpan) calibrationValueSpan.textContent = config.calibration.toString();
+
+  setInputValue('rdsModes', config.rdsModeIdx.toString());
   setInputValue('utcoffset', config.utcOffsetIdx.toString());
+  setInputValue('fmRegions', config.fmRegionIdx.toString());
   setInputValue('theme', config.themeIdx.toString());
-  setCheckboxValue('scroll', config.scrollDirection === -1);
+  setInputValue('uiLayouts', config.uiLayoutIdx.toString());
   setCheckboxValue('zoom', config.zoomMenu);
+  setCheckboxValue('scroll', config.scrollDirection === -1);
+  setInputValue('sleepModes', config.sleepModeIdx.toString());
 }
 
 const saveButton = byId('saveButton');
@@ -48,12 +68,20 @@ if (saveButton) {
   });
 }
 
+syncValues('brightness', 'brightnessValue');
+syncValues('calibration', 'calibrationValue');
+
 Promise.all([configOptionsApi(), configApi()])
   .then(([configOptions, config]: [ConfigOptions, Config]) => {
+    populateSelect('rdsModes', configOptions.rdsModes.map(t => ({value: t.id.toString(), label: t.desc})));
     populateSelect('utcoffset', configOptions.UTCOffsets.map(o => ({
       value: o.id.toString(),
       label: `${o.city} (${o.desc})`
     })));
+    populateSelect('fmRegions', configOptions.fmRegions.map(t => ({value: t.id.toString(), label: t.desc})));
     populateSelect('theme', configOptions.themes.map(t => ({value: t.id.toString(), label: t.name})));
+    populateSelect('uiLayouts', configOptions.uiLayouts.map(t => ({value: t.id.toString(), label: t.name})));
+    populateSelect('sleepModes', configOptions.sleepModes.map(t => ({value: t.id.toString(), label: t.name})));
+
     populateConfig(config);
   })
